855da89b0d19ce91c62332d47afd8d357e957660
Kirilloao <kirilltaraturin747@gmail>
2023-08-27 16:37:31 +0200

    Initial Commit

---
+++ LinkedList/LinkedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedListApp.swift
+//  LinkedList
+//
+//  Created by Kirill Taraturin on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinkedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
